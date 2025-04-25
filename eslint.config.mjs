2cf161be9186a6ab3ace065fab99b0d7425ec265
ljuba_ch <ljuba650@gmail.com>
2025-04-25 17:59:33 +0300

    added prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 import jest from "eslint-plugin-jest";
+import eslintRecommended from "eslint-plugin-prettier/recommended";
 
 
 export default defineConfig([
+  eslintRecommended,
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   {
